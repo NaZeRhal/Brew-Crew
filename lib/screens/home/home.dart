a13eb9c7b6c1e05f8867b26cbeeee7f978a91e72
NAZERHAL <Mo66Bu61Ht27Ig75>
2020-11-02 21:10:36 +0300

    add: assets, images

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -48,7 +48,15 @@ class Home extends StatelessWidget {
             )
           ],
         ),
-        body: BrewList(),
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/coffee_bg.jpg'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: BrewList(),
+        ),
       ),
     );
   }
